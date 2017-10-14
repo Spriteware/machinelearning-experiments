b7c25652111caad8812dc077e163d6cb0b3f3532
Spriteware <andrieux.francois@gmail.com>
2017-10-14 19:21:34 +0200

    Improved mouse experiment experience & trainijng data saving

--- mouse_experiment.js
+++ mouse_experiment.js
@@ -1,8 +1,28 @@
+"use strict";
+
+const _TRAINING_SIZE_MAX = 10000;
 const _CANVAS_WIDTH  = 1000;
 const _CANVAS_HEIGHT = 600;
-const _POINT_RADIUS  = 8;
+const _CIRCLE_RADIUS = 50;
+const _POINT_RADIUS = 8;
 
-const _EPOCHS = 2000;
+const _epochs = 200;
+const _params = {
+    libURI: "http://localhost/machinelearning/neural-network.js",
+    hiddenLayerFunction: "linear",
+    momentum: 0.0,
+    lr: 0.001,
+    layers: [2, 3, 3, 2],
+
+    // layers: [2, 4, 4, 4, 2],
+    // layers: [2, 5, 1, 5, 2]
+    // layers: [2, 15, 15, 15, 2]
+    // layers: [2, 3, 4, 5, 6, 5, 4, 3, 2]
+    // layers: [2, 40, 2]
+    // Find additionnal good hyperparameters configurations below in the code
+};
+
+//////////////////////
 
 var Utils = {
     static: {}
@@ -10,15 +30,16 @@ var Utils = {
 
 Utils.static.exportTrainingData = function() {
 
+    // Fonction used in console
     console.info("Saving training data...", "Reading 'training_data'");
 
     var output = document.createElement("textarea");
     output.setAttribute("disabled", "disabled");
-    output.innerHTML = "var training_data_imported = " + JSON.stringify(training_data, null, '\t') + ";";
+    output.innerHTML = "var training_data_imported = \"" + training_data + "\";";
 
     document.body.appendChild( output );
 
-    return "Export completed for " + training_data.length + " entries.";
+    return "Export completed for " + training_size + " entries.";
 };
 
 function init() {
@@ -43,8 +64,9 @@ function init() {
         
         // Initial training
         if (typeof training_data_imported !== 'undefined' && training_data_imported !== undefined)
-            DOM.trainButton.parentElement.appendChild( brain.train(training_data_imported, _EPOCHS, true) ); // second parameter is number of epochs
-
+            DOM.trainButton.parentElement.appendChild( brain.train(training_data_imported, _epochs, true) ); // second parameter is number of epochs
+        else 
+            alert("No training data available");
     });
 
     window.addEventListener("keydown", function(e) {
@@ -61,7 +83,6 @@ function init() {
 function update() {
 
     requestAnimationFrame(function() { update(); });
-
     ctx.clearRect(-_CANVAS_WIDTH / 2, -_CANVAS_HEIGHT / 2, _CANVAS_WIDTH, _CANVAS_HEIGHT);
     
     ///////////////////// OWN LIBRAIRY JS //////////////////
@@ -69,14 +90,13 @@ function update() {
     // Feeforward NN
     var inputs = [mouse.x / norm_x, mouse.y / norm_y];
     var targets = [mouse.x / norm_x, mouse.y / norm_y];
-    // var targets = [mouse.x, mouse.y];
     var neurons = brain.feed(inputs);
 
-    // Build training data for future exportation
-    training_data.push({
-        inputs: inputs,
-        targets: targets
-    });
+    // Build training data (as string) for future exportation
+    if (training_size <= _TRAINING_SIZE_MAX) {
+        training_data += inputs[0] + " " + inputs[1] + " : " + targets[0] + " " + targets[1] + "\\\n"; 
+        training_size++;
+    }
 
     if (DOM.backpropagationCheckbox.checked === true)
         brain.backpropagate(targets);
@@ -88,7 +108,7 @@ function update() {
 
     // Draw circle
     ctx.beginPath();
-    ctx.arc(neurons[0].output * norm_x, neurons[1].output * norm_y, 50, 0, Math.PI * 2, false);
+    ctx.arc(neurons[0].output * norm_x, neurons[1].output * norm_y, _CIRCLE_RADIUS, 0, Math.PI * 2, false);
     ctx.stroke();
 
     // Update global error display
@@ -98,8 +118,7 @@ function update() {
     brain.visualize(inputs);
 }
 
-var DOM, ctx, mouse, brain, training_data = [];
-var norm = Math.sqrt(_CANVAS_WIDTH * _CANVAS_WIDTH + _CANVAS_HEIGHT * _CANVAS_HEIGHT) / 2;
+var DOM, ctx, mouse, brain, training_data = "", training_size = 0;
 var norm_x = _CANVAS_WIDTH / 2;
 var norm_y = _CANVAS_HEIGHT / 2;
 
@@ -116,45 +135,29 @@ window.onload = function() {
     ctx = DOM.canvas.getContext("2d");
 
     mouse = {x: 1, y: 2, refresh: false};
-    brain = new Network({
-        momentum: 0.0,
+    brain = new Network(_params);
 
-        lr: 0.001,
-        layers: [2, 3, 3, 2],
-        hiddenLayerFunction: "linear",
-        
-        // # good-config 1:
-        // lr: 0.005, // we can up to 0.1
-        // layers: [2, 3, 2],
-        // hiddenLayerFunction: "linear",
-        
-        
-        // #good-config 2:
-        // lr: 0.0005,
-        // layers: [2, 4, 4, 4, 2],
-        // hiddenLayerFunction: "linear",
+    // # good-config 1:
+    // lr: 0.005, // we can up to 0.1
+    // layers: [2, 3, 2],
+    // hiddenLayerFunction: "linear",
+    
+    // #good-config 2:
+    // lr: 0.0005,
+    // layers: [2, 4, 4, 4, 2],
+    // hiddenLayerFunction: "linear",
 
-        // #not-so-good-but-okay-config 3: (using tanh)
-        // lr: 0.04,
-        // layers: [2, 4, 2], // layers ddoesn't change things too much
-        // hiddenLayerFunction: "tanh",
+    // #not-so-good-but-okay-config 3: (using tanh)
+    // lr: 0.04,
+    // layers: [2, 4, 2], // layers ddoesn't change things too much
+    // hiddenLayerFunction: "tanh",
 
-        // #good-config 4, epochs=200
-        // lr: 0.001,
-        // layers: [2, 6, 6, 6, 6, 6, 6, 6, 6, 6, 2],
-        // hiddenLayerFunction: "linear",
-        
-        // layers: [2, 4, 4, 4, 2],
-        // layers: [2, 10, 2],
-        // layers: [2, 6, 6, 6, 6, 6, 6, 6, 6, 6, 2]
-        // layers: [2, 5, 1, 5, 2]
-        // layers: [2, 15, 15, 15, 2]
-        // layers: [2, 3, 3, 3, 3, 3, 3, 2]
-        // layers: [2, 3, 4, 5, 6, 5, 4, 3, 2]
-        // layers: [2, 40, 2]
-    });
+    // #good-config 4, epochs=200
+    // lr: 0.001,
+    // layers: [2, 6, 6, 6, 6, 6, 6, 6, 6, 6, 2],
+    // hiddenLayerFunction: "linear",
 
-    // Setting activation function for hiddens layer:
+    // Update activation function for hiddens layer:
     // brain.setHiddenLayerToActivation(brain.static_tanhActivation, brain.static_tanhDerivative);
     // brain.setHiddenLayerToActivation(brain.static_sigmoidActivation, brain.static_sigmoidDerivative);
     // brain.setHiddenLayerToActivation(brain.static_reluActivation, brain.static_reluDerivative);
