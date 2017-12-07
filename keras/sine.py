efc6fc16a7e9ff4790f0d27c0d3bdbf7a752977f
Spriteware <andrieux.francois@gmail.com>
2017-12-07 14:55:31 +0100

    keras experiments updated

--- keras/sine.py
+++ keras/sine.py
@@ -21,28 +21,22 @@ def split_set(dataset):
     y = np.array([dataset[2]]).transpose()
     return X, y
 
-def function(x):
-    return np.sin(x / 10) / 2
+def sine(x):
+    return np.sin(x / 15) / 2
 
 ###############################
 
 
-def generate_data(sequences, sequences_size, display=True):
+def generate_data(sequences, sequences_size, padding=0.0):
 
     # Generating and displaying data
-    raw = np.fromfunction(function, (sequences - 1 + sequences_size,), dtype=int)
+    raw = np.fromfunction(lambda x: sine(x+padding), (sequences - 1 + sequences_size,), dtype=int)
     X = []
     y = []
 
     for i in range(sequences):
         X.append(raw[i : i + sequences_size - 1])
         y.append(raw[i + sequences_size - 1 : i + sequences_size])
-
-    if display is True:
-        fig = plt.figure()
-        for i in range(sequences):
-            plt.plot(np.arange(sequences_size-1) + i, X[i] + i *0.01)
-        plt.show()
 
     return np.asarray(X), np.asarray(y)
 
@@ -58,23 +52,66 @@ def build_model(dim):
     callback = TensorBoard(log_dir=path)
     sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 
-    model.add(layers.LSTM(units=dim[1], input_shape=(None, dim[0])))
+    model.add(layers.LSTM(
+        units=dim[1], 
+        input_shape=(dim[0], 1),
+        return_sequences=True))
+    model.add(layers.LSTM(
+        units=dim[2],
+        return_sequences=False))
     model.add(layers.Dropout(0.2))
-    model.add(layers.Dense(units=dim[2]))
+    model.add(layers.Dense(
+        units=1))
     model.add(layers.Activation("linear"))
 
     start = time.time()
-    model.compile(loss="mse", optimizer="rmsprop")
+    model.compile(loss="mse", optimizer="rmsprop", metrics=["accuracy"])
     print("> Compilation Time : ", time.time() - start)
 
     return model, callback
 
 ###############################
 
-seq = 10
-seq_size = 100
+seqs = 50
+seq_size = 200
+test_samples = 300
+# test_padding = np.random.random_sample() * seq_size
+test_padding = -seq_size
 
-X, y = generate_data(seq, seq_size, False)
-model, callback = build_model([seq_size-1, 25, 1])
+print("> {} sequences (size={}), {} test samples with padding={} Time : ".format(seqs, seq_size, test_samples, test_padding))
 
-model.fit(X, y, batch_size=128, epochs=1, callbacks=[callback])
+# Data
+x_train, y_train = generate_data(seqs, seq_size)
+x_test, y_test = generate_data(test_samples, seq_size, test_padding)
+
+x_train_copy = np.copy(x_train)
+x_test_copy = np.copy(x_test)
+
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+print("x_train", x_train.shape, x_train)
+print()
+print("x_test", x_test.shape, x_test)
+
+# Model
+model, callback = build_model([seq_size-1, 100, 1])
+model.fit(x_train, y_train, batch_size=16, epochs=1, callbacks=[callback])
+
+# Prediction
+predicted = model.predict(x_test)
+print ("> Predicted {}: {}".format(predicted.shape, predicted))
+
+# Display
+fig = plt.figure()
+
+for i in range(test_samples):
+    plt.plot(np.arange(seq_size - 1) + i + test_padding, x_test_copy[i], alpha=0.3)
+    plt.plot(seq_size + i + test_padding, predicted[i][0], "or", markersize=2)
+
+for i in range(seqs):
+    plt.plot(np.arange(seq_size - 1) + i, x_train_copy[i], color="black")
+
+plt.show()
+
+# print(predicted)
---
+++ keras/sine_lstm.py
@@ -0,0 +1,117 @@
+#! C:/Python3/pythonw.exe -u
+
+import os
+import sys
+import csv
+import time
+import numpy as np
+
+from keras.models import Sequential
+from keras.callbacks import TensorBoard, EarlyStopping
+from keras.optimizers import SGD
+from keras.layers import LSTM, Dropout, Dense, Activation
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"  # Hide messy TensorFlow warnings
+
+def dir_count(path):
+    for curr, dirnames, filenames in os.walk(path):
+        return len(dirnames)
+
+def split_set(dataset):
+    X = np.array([dataset[0], dataset[1]]).transpose()
+    y = np.array([dataset[2]]).transpose()
+    return X, y
+
+def sine(x):
+    return np.sin(x / 20) / 2
+
+###############################
+
+
+def generate_data(dataset_size, sequence_size, padding=0.0):
+    
+    if dataset_size <= sequence_size:
+        raise ValueError("The dataset size can't be smaller or equal to the sequence size")
+
+    # Generating and displaying data
+    raw = np.asarray(np.fromfunction(lambda x: sine(x+padding), (dataset_size,), dtype=int)).tolist()
+    data = []
+    i = sequence_size
+
+    # We shift our window (size by sequence_size) of 1 on the right. i is the right edge of the window
+    while True:
+        data.append( raw[i - sequence_size: i+1] )
+
+        if i >= dataset_size-1:
+            break
+        i += 1
+
+    # Data shuffling
+    data = np.asarray(data)
+    np.random.shuffle(data)
+
+    data = data.transpose()
+    y1d = np.copy(data[sequence_size])
+    x2d = np.delete(data, sequence_size, 0).transpose()
+
+    print("FIRST x2d", x2d.shape, x2d, "\n")
+    print("FIRST y1d", y1d.shape, y1d, "\n")
+
+    y1d = y1d.reshape((y1d.shape[0], 1))
+    x2d = x2d.reshape((x2d.shape[0], x2d.shape[1], 1))
+
+    # print("SECOND x2d", x2d.shape, x2d, "\n")
+    # print("SECOND y1d", y1d.shape, y1d, "\n")
+
+    return raw, x2d, y1d
+
+
+
+class BatchHistory(TensorBoard):
+
+    def on_epoch_begin(self, epoch, logs=None):
+        self.curr_epoch = epoch
+    
+    def on_batch_end(self, batch, logs={}):
+        self.on_epoch_end(self.curr_epoch, logs)
+    
+
+def build_model(dim):
+    
+    # Computing the right folder for TensorBoard
+    path = "./tmp/" + str(dir_count("./tmp"))
+    print("> Saving in '{}'".format(path));
+
+    # Building model
+    model = Sequential()
+    sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+    tensorboard = TensorBoard(log_dir=path, write_grads=True, write_images=True)
+    early_stopping = EarlyStopping(monitor="val_loss")
+    callbacks = [tensorboard, early_stopping]
+    # callback = BatchHistory(log_dir=path)
+
+    model.add(LSTM(
+        units=dim[1],
+        input_shape=(dim[1], dim[0]),
+        activation=None,
+        return_sequences=True,
+        implementation=1))
+    model.add(Dropout(0.2))
+
+    model.add(LSTM(
+        units=dim[2],
+        activation=None,
+        return_sequences=False,
+        implementation=1))
+    model.add(Dropout(0.2))
+
+    model.add(Dense(
+        units=dim[3], 
+        activation="linear"))
+    model.add(Activation("linear"))
+
+    start = time.time()
+    model.compile(loss="mse", optimizer=sgd)
+    print("> Compilation Time : ", time.time() - start)
+
+    return model, callbacks
